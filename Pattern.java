d4be0f7aa239d3251724c68867572ff4e898d7ea
Shailesh393 <128904820+Shailesh393@users.noreply.github.com>
2023-05-19 18:32:07 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+//Pattern_Neetika Ma'am
+
+public class Pattern{
+  public static void main(String args[]){
+     int i, j;
+     char a[] ={'N','E','E','T','I','K','A'};
+      for(i=0;i<=7;i++){
+           for(j=0;j<i;j++){
+               System.out.print(a[j]);
+             }
+              System.out.print("\n ");
+             }
+         }
+     }
